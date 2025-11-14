128ad020c1afa643dc479479ecef7c5fef494c60
Hussain Jamal <Hjamal9865@gmail.com>
2025-11-14 05:20:35 +0500

    activate token done

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-import {registrationUser} from '../controllers/user.controllers'
+import {activateUser, registrationUser} from '../controllers/user.controllers'
 const userRouter = express.Router();
 
 userRouter.post('/registration',registrationUser);
+userRouter.post('/activate-user',activateUser);
+
 
 export default userRouter;
