da4eac1eb95b915402930da3380e44433142e740
Hussain Jamal <Hjamal9865@gmail.com>
2025-11-18 03:59:48 +0500

    get all and single courses added

--- server/controllers/course.controllers.ts
+++ server/controllers/course.controllers.ts
@@ -67,3 +67,31 @@ export const editCourse = CatchAsyncError(async(req:Request,res:Response,next:Ne
         return next(new ErrorHandler(error.message , 400))
     }
 })
+
+
+
+export const getAllCourses = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const course = await CourseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.question -courseData.links")
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message , 400))
+    }
+})
+
+export const getSingleCourse = CatchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.question -courseData.links")
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message , 400))
+    }
+})
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import {isAuthenticated,authorizeRoles} from '../middleware/auth'
-import { uploadCourse ,editCourse} from '../controllers/course.controllers';
+import { uploadCourse ,editCourse, getSingleCourse , getAllCourses} from '../controllers/course.controllers';
 const courseRouter = express.Router();
 
 courseRouter.post('/create-course',isAuthenticated , authorizeRoles("admin"),uploadCourse);
 courseRouter.put('/edit-course/:id',isAuthenticated , authorizeRoles("admin"),editCourse);
+courseRouter.get('/get-course/:id',getSingleCourse);
+courseRouter.get('/get-courses',getAllCourses);
 
 export default courseRouter
