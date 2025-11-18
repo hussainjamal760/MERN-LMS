63b1f428450104488599b97aedcbfc49cb0041f2
Hussain Jamal <Hjamal9865@gmail.com>
2025-11-19 04:25:28 +0500

    notification model added

---
+++ server/models/notification.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface INotification extends Document{
+    userId:string,
+    message:string,
+    status:string,
+    title:string,
+}
+
+const notificationSchema = new Schema<INotification>({
+    title:{
+        type:String,
+        required:true
+    },
+    message:{ 
+        type:String,
+        required:true
+    },
+    status:{
+         type:String,
+         required:true,
+         default:"unread"
+    }
+},{timestamps:true})
+
+const NotificationModel:Model<INotification> = mongoose.model("Order" , notificationSchema)
+export default NotificationModel
