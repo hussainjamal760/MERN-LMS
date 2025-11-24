063d742103937e395add4ea5b9c74ddd59447b25
Hussain Jamal <Hjamal9865@gmail.com>
2025-11-25 04:20:12 +0500

    config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... any existing config here ...
   images: {
     remotePatterns: [
       {
@@ -9,21 +8,10 @@ const nextConfig = {
         port: '',
         pathname: '/**'
       },
-      // You may also need to add other external domains like Google/GitHub avatars if not done already
-      // {
-      //   protocol: 'https',
-      //   hostname: 'avatars.githubusercontent.com',
-      //   port: '',
-      //   pathname: '/**'
-      // }
-      // {
-      //   protocol: 'https',
-      //   hostname: 'lh3.googleusercontent.com',
-      //   port: '',
-      //   pathname: '/**'
-      // }
+   
+
     ],
   },
 };
 
-export default nextConfig; // Ya 'module.exports = nextConfig;' agar .js file ho
+export default nextConfig; 
