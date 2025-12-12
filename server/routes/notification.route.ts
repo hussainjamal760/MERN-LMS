fd71d700928b0b406e25f2c40fb3d4edde1bc7f1
Hussain Jamal <Hjamal9865@gmail.com>
2025-12-13 04:23:09 +0500

    small change

--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -3,7 +3,7 @@ import {isAuthenticated,authorizeRoles} from '../middleware/auth'
 import { getNotification, updateNotification } from '../controllers/notification.controller';
 const notificationRouter = express.Router();
 
-notificationRouter.get("/get-notifications" , isAuthenticated ,authorizeRoles("admin") , getNotification)
-notificationRouter.put("/update-notifications/:id" , isAuthenticated ,authorizeRoles("admin") , updateNotification)
+notificationRouter.get("/get-notifications"  , getNotification)
+notificationRouter.put("/update-notifications/:id"  , updateNotification)
 
 export default notificationRouter
