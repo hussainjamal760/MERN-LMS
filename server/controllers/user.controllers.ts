231101ea209733660812821af368e1fbf8170d0b
Hussain Jamal <Hjamal9865@gmail.com>
2026-01-01 01:46:09 +0500

    REMOVING MAIL

--- server/controllers/user.controllers.ts
+++ server/controllers/user.controllers.ts
@@ -46,24 +46,24 @@ const html = await ejs.renderFile(
 );
 
 
-        try {
-            await sendMail({
-                email:user.email,
-                subject:"Activate your account",
-                template:'activation-mail.ejs',
-                data,
-            })
+//         try {
+//             await sendMail({
+//                 email:user.email,
+//                 subject:"Activate your account",
+//                 template:'activation-mail.ejs',
+//                 data,
+//             })
 
-            res.status(201).json({
-            success: true,
-            message: `Please check your mail ${user.email} to activate your account`,
-            activationToken: activationToken.token,
-})
+//             res.status(201).json({
+//             success: true,
+//             message: `Please check your mail ${user.email} to activate your account`,
+//             activationToken: activationToken.token,
+// })
 
             
-        } catch (error:any) {
-            return next(new ErrorHandler(error.message ,400))
-        }
+//         } catch (error:any) {
+//             return next(new ErrorHandler(error.message ,400))
+//         }
 
     } catch (error:any) {
         return next(new ErrorHandler(error.message,400))
