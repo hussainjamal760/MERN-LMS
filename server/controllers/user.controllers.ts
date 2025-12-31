3406931de528dc61a12b421210f1afc291f5ee35
Hussain Jamal <Hjamal9865@gmail.com>
2025-12-31 23:15:19 +0500

    fixing mail issue

--- server/controllers/user.controllers.ts
+++ server/controllers/user.controllers.ts
@@ -40,7 +40,7 @@ export const registrationUser = CatchAsyncError(async(req:Request , res:Response
 
         const data = {user: {name : user.name} , activationCode};
 
-        const html = await ejs.renderFile(path.join(__dirname,"../mails/activation-mail.ejs"), data )
+const html = await ejs.renderFile(path.join(process.cwd(), "server/mails/activation-mail.ejs"), data)
 
         try {
             await sendMail({
